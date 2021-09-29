d3830f0412a68e8d3e7f7795a31d59a2c60283b5
millenys4ntos <millenydossantos10.msc@gmail.com>
2021-09-29 22:18:47 +0900

    .

--- www/components/script.js
+++ www/components/script.js
@@ -3,57 +3,81 @@ window.onload = function(){
 
  document.querySelector("#direita").addEventListener("click", function(){
    direita();
+   setTimeout(parar, 1000);  
  });
 
  document.querySelector("#esquerda").addEventListener("click", function(){
    esquerda();
+   setTimeout(parar, 1000);
  });
 
  document.querySelector("#subir").addEventListener("click", function(){
    subir();
+   setTimeout(parar, 1000); 
  });
 
  document.querySelector("#descer").addEventListener("click", function(){
    descer();
+   setTimeout(parar, 1000); 
  });
 }
 
 var personagemObj;
-var obstaculo;
+var obstaculo = [];
+var pontos;
 
 function inicioJogo(){
   areaJogo.start();
   personagemObj = new componente("#F00",10,100,30,30);
-  obstaculo = new componente('green',150,80,120,10);
+  pontos = new componente("#000", 10,30,'Consolas','30px', 'texto');
+  //obstaculo = new componente('green',150,80,120,10);
 }
 
 let areaJogo = {
   canvas: document.createElement("canvas"),
-  start: function() {
+  start: function(){
     this.canvas.height = 300,
     this.context = this.canvas.getContext("2d");
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-    this.intervalo = setInterval(atualizaAreaJogo,  20);
+    this.frame = 0;
+    this.intervalo = setInterval(atualizaAreaJogo, 20);
   },
   limpar: function(){
-    this.context.clearReact(0,0, this.canvas.width, this.canvas.height);
+    this.context.clearRect(0,0, this.canvas.width, this.canvas.height);
   },
   parar: function(){
     clearInterval(this.interval); 
   }
 }
 
-function componente(cor, x, y, largura, altura){
+function contarIntervalo(n){
+  if((areaJogo.frame / n) %  1 == 0){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+function componente(cor, x, y, largura, altura, tipo){
+  this.tipo = tipo,
   this.altura = altura,
   this.largura = largura,
   this.x = x,
   this.y = y,
   this.velocidadeX = 0,
   this.velocidadeY = 0,
-  this.atualiza= function(){
+  this.texto = 0;
+  this.atualiza = function(){
     contexto = areaJogo.context;
-    contexto.fillStyle = cor,
-    contexto.fillRect(this.x, this.y, this.altura, this.largura);
+    if(this.tipo == "texto"){
+      contexto.font = this.altura + " " + this.largura;
+      contexto.fillStyle = cor;
+      contexto.fillText(this.texto, this.x, this.y);
+    }else{
+      contexto.fillStyle = cor,
+        contexto.fillRect(this.x, this.y, this.altura, this.largura);
+    }
+    
   },
   this.novaPosicao = function(){
     this.x += this.velocidadeX;
@@ -82,19 +106,43 @@ function componente(cor, x, y, largura, altura){
     }
     return batida;
   }
- 
 }
 
 function atualizaAreaJogo(){
-  if(personagemObj.bater(obstaculo)){
+  let x, y;
+
+  for(i = 0; i < obstaculo.length; i++){
+   if(personagemObj.bater(obstaculo[i])){
     areaJogo.parar();
-  }else{
+    return;
+   }
+ }
+  
     areaJogo.limpar();
-    obstaculo.atualiza();
+    areaJogo.frame += 1;
+
+    if(areaJogo.frame == 1 || contarIntervalo(150)){
+      x = areaJogo.canvas.width;
+      minAltura = 20;
+      maxAltura = 200;
+      altura = Math.floor(Math.random()*(maxAltura-minAltura+1)+minAltura);
+      minVazio = 50;
+      maxVazio = 200;
+      vazio = Math.floor(Math.random()*(maxVazio-minVazio+1)+minVazio);
+      obstaculo.push(new componente('green',x,0,altura,10));
+      obstaculo.push(new componente('green',x,altura + vazio,x - altura - vazio,10));
+    }
+
+    for(i = 0; i < obstaculo.length; i++){
+      obstaculo[i].x += -1;
+      obstaculo[i].atualiza();
+    }
+    pontos.texto = "Pontos: " + areaJogo.frame;
+    pontos.atualiza();
     personagemObj.novaPosicao();
     personagemObj.atualiza();
   }
-}
+
 
 function subir(){
   personagemObj.velocidadeY -= 1;
@@ -110,4 +158,9 @@ function direita(){
 
 function esquerda(){
   personagemObj.velocidadeX -= 1;
+}
+
+function parar(){
+  personagemObj.velocidadeX = 0;
+  personagemObj.velocidadeY = 0;
 }
